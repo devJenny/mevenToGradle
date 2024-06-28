8e124a43cb74809c5aed7f0f7c46129c1bb2d934
devjenny <devje@naver.com>
2024-06-28 12:31:13 +0900

    Feat: `SecurityConfig` 추가

---
+++ src/main/java/com/crud/home/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package com.crud.home.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public BCryptPasswordEncoder encode() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests((authorizeRequests) -> // 인증, 인가 설정
+                        authorizeRequests
+                                .requestMatchers( "/", "/board/**").authenticated()
+                                .anyRequest().permitAll()
+                )
+                .formLogin((formLogin) -> // 폼 기반 로그인 설정
+                        formLogin
+                                .loginPage("/auth/login") // get
+                                .loginProcessingUrl("/auth/login") // post -> 스프링 시큐리티가 로그인 프로세스 진행
+                                .defaultSuccessUrl("/")
+                )
+                .csrf((csrfConfig) -> // csrf 비활성화
+                        csrfConfig.disable()
+                )
+                .build();
+    }
+}

dd55b33336ad59a6b23ffea97ca485f41f24b428
devjenny <devje@naver.com>
2024-07-05 18:09:04 +0900

    Remove: comment 관련 삭제

--- src/main/java/com/crud/home/Repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.crud.home.Repository;
-
-import com.crud.home.Entity.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
